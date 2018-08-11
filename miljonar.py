ab0458f61568ff60858f78b63cc302a0f5899944
evadezelak <eva.dezelak@gmail.com>
2018-08-11 03:44:47 +0200

    glas ljudstva
    dodana nova funkcionalnost glas ljudstva

--- miljonar.py
+++ miljonar.py
@@ -70,6 +70,7 @@ class grafika:
 
    def pozdravno_okno(self, zacetek, glasba):
       ozadje = tk.PhotoImage(file = "ozadja/drugo_ozadje.gif")
+      self.ozadje = ozadje
       ozadje_s_sliko = tk.Label(zacetek, image = ozadje)
       ozadje_s_sliko.place(x = 0, y = 0, relwidth = 1, relheight = 1)
       pozdrav = tk.Label(zacetek, wraplength = 1000, text = "Pozdravljeni v igri Lepo je biti Milijonar! Igra je sestavljena iz treh nivojev težavnosti. Po vsakem doseženem nivoju, se lahko odločite, ali boste denar vzeli, ali igrali naprej.Veliko sreče!!", fg = "white", bg = "grey6", font = ("Comic Sans MS", 30) )
@@ -108,22 +109,25 @@ class grafika:
       vzemi.mainloop()
       
    def poraz(self,zacetek, okvir_za_gumbe):
+      
+     # Grid.columnconfigure(zacetek,1, weight = 1)
       okvir_za_gumbe.destroy()
       self.uniči_gumbe(self.seznam_widgetov)
       ozadje = tk.PhotoImage(file = "ozadja/drugo_ozadje.gif")
       ozadje_s_sliko = tk.Label(zacetek, image = ozadje)
       ozadje_s_sliko.place(x = 0, y = 0, relwidth = 1, relheight = 1)
       koncaj = tk.Label(zacetek, text = "Ostali ste brez nagrade, več sreče prihodnjič!", fg = "white", bg = "grey6", font = ("Comic Sans MS", 40))
-      koncaj.grid(row = 0, pady = 300 )
-      izhod1 = tk.Button(zacetek, text = "Izhod", fg = "white", bg = "grey6", font = ("Comic Sans MS", 30), command = lambda: grafika.izhod__(self, zacetek))
-      izhod1.grid(sticky = W, row = 1)
-      ponovna_igra = tk.Button(zacetek, text = "Poskusi znova", fg = "white", bg = "grey6", font = ("Comic Sans MS", 30), command = lambda : grafika.pred_ponovno_igro_uniči_prejšne_widgete(self, zacetek))
-      ponovna_igra.grid(sticky = E , row = 1)
+      koncaj.grid(sticky = N, row = 0, columnspan = 2, pady = 50 )
+      izhod1 = tk.Button(zacetek, text = "Izhod", fg = "white", bg = "grey6", font = ("Comic Sans MS", 30), command = lambda: grafika.izhod__(self, zacetek), width = 15, height = 2)
+      izhod1.grid(sticky = W , row = 0, column = 0, padx = 300)
+      ponovna_igra = tk.Button(zacetek, wraplength = 200 , text = "Poskusi znova", fg = "white", bg = "grey6", width = 15 ,height = 2,  font = ("Comic Sans MS", 30), command = lambda : grafika.pred_ponovno_igro_uniči_prejšne_widgete(self, zacetek))
+      ponovna_igra.grid(sticky = E  ,  row = 0, column = 1, padx = 300)
       self.dodaj_na_seznam_widgetov(ozadje_s_sliko, koncaj, izhod1, ponovna_igra)
       zacetek.mainloop()
 
    def pred_ponovno_igro_uniči_prejšne_widgete(self, zacetek):
       self.uniči_gumbe(self.seznam_widgetov)
+      #Grid.columnconfigure(zacetek,0, weight = 1)
       prva(zacetek)
 
    def vrni_pravilni_odgovor(self, vprasanje_in_odgovori):
@@ -253,20 +257,90 @@ class grafika:
       self.podatki.polovicka = False
       gumb.config(state = "disabled")
 
-   #def poracunaj_procente(pravilni_odgovor, vprasanje_in_odgovori):
+   def poisci_pravi_gumb( pravilni_odgovor):
+      for gumb in grafika.seznam_gumbov:
+         if(pravilni_odgovor in gumb['text']):
+            odgovor = gumb['text']
+      pravilna_crka = odgovor[0]
+      return pravilna_crka
+   
+   def izdelaj_seznam_parov(seznam_vseh, pravilen_par):
+      prejsnja_vrednost = -1
+      indeks_para = 0
+      for par in seznam_vseh:
+         if(par[0] == pravilen_par[0]):
+            prejsnja_vrednost = par[1]
+            novi_par =(pravilen_par[0], pravilen_par[1])
+            seznam_vseh[indeks_para] = novi_par
+         indeks_para+=1
+      indeks_para = 0
+      for par in seznam_vseh:
+         if(par[1] == pravilen_par[1] and par[0] != pravilen_par[0]):
+            novi_par =(par[0], prejsnja_vrednost)
+            seznam_vseh[indeks_para] = novi_par
+            return seznam_vseh
+         indeks_para+=1
+      return seznam_vseh
+   
+   def poracunaj_procente(pravilni_odgovor):
+      prvi_odstotek = random.randint( 0, 100 )
+      drugi_odstotek = 100 - random.randint( prvi_odstotek, 100)
+      tretji_odstotek = 100 - random.randint( prvi_odstotek + drugi_odstotek , 100 )
+      cetrti_odstotek = 100 - (prvi_odstotek + drugi_odstotek + tretji_odstotek)
+      seznam_vseh = [('A', prvi_odstotek), ('B', drugi_odstotek), ('C', tretji_odstotek), ('D', cetrti_odstotek)]
+      najvisji_procent = max(prvi_odstotek, drugi_odstotek, tretji_odstotek, cetrti_odstotek)
+      pravilni_odgovor = grafika.poisci_pravi_gumb(pravilni_odgovor)
+      pravilen_par = (pravilni_odgovor, najvisji_procent)
+      print(prvi_odstotek, drugi_odstotek, tretji_odstotek, cetrti_odstotek, najvisji_procent, pravilni_odgovor)
+      seznam_parov = grafika.izdelaj_seznam_parov(seznam_vseh, pravilen_par)
+      print(seznam_parov)
+      return seznam_parov
+
+   def izrisi_graf(self, seznam_parov, gumb):
+      glas_ljudstva_okno = tk.Tk()
+      glas_ljudstva_okno.title("Glas ljudstva")
+      glas_ljudstva_okno.geometry('%dx%d+%d+%d' % (700, 500, self.sirina_okna/2 - 700/2, self.visina_okna/2 - 500/2))
+      sirina_podlage = 700
+      visina_podlage = 500
+      platno = tk.Canvas(glas_ljudstva_okno, width = sirina_podlage, height = visina_podlage)
+      platno.pack(fill ='both', expand = 'yes')
+      ozadje = tk.PhotoImage(master = platno, file = "ozadja/drugo_ozadje.gif")
+      platno.create_image(0,0 ,image = ozadje, anchor = 'center')
+      y_razteg = 4
+      y_razmak = 50
+      x_sirina = 25
+      x_razteg = sirina_podlage/4 - x_sirina
+      x_razmak= 20
+
+      for x, par in enumerate(seznam_parov):
+         x0 = x * x_razteg + x * x_sirina + x_razteg
+         y0 = visina_podlage - (par[1] * y_razteg + y_razmak)
+         x1 = x * x_razteg + x * x_sirina + x_sirina + x_razmak
+         y1 = visina_podlage - y_razmak
+         platno.create_rectangle(x0, y0, x1, y1, fill = "grey6", outline = "silver")
+         platno.create_text(x0 - 55, y0 - 40, anchor = tk.N ,text =  str(par[1])+ " %", font = ("Comic Sans MS", 20), fill = "white")
+         platno.create_text(x0 - 55, y1 + 40, anchor = tk.S ,text =  str(par[0])+ ")", font = ("Comic Sans MS", 20), fill = "white")
+         platno.create_line(0, visina_podlage - 48, sirina_podlage, visina_podlage - 48, fill = "silver", width = 1.5)
+      gumb.config(state = "disabled")
+      self.podatki.glas_ljudstva = False
+      self.seznam_widgetov.append(glas_ljudstva_okno)
+      glas_ljudstva_okno.mainloop()
+      
       
    
    def izkoristi_glas_ljudstva(self, vprasanje_in_odgovori, gumb):
       pravilni_odgovor = grafika.vrni_pravilni_odgovor(self, vprasanje_in_odgovori)
-      grafika.poracunaj_procente(pravilni_odgovor, vprasanje_in_odgovori)
-      self.podatki.glas_ljudstva = False
-      gumb.config(state = "disabled")
+      seznam_parov = grafika.poracunaj_procente(pravilni_odgovor)
+      grafika.izrisi_graf(self, seznam_parov, gumb)
+     
+      
       
    def naredi_gumb(self, st_odgovora, vprasanje_in_odgovori,   zacetek, okvir_za_gumbe):
+         crke = ['A', 'B', 'C', 'D']
          gumb = tk.Button(okvir_za_gumbe)
          besedilo = vprasanje_in_odgovori[st_odgovora].strip('***\n')
          ukaz = lambda: grafika.preveri_odgovor(self, vprasanje_in_odgovori,zacetek, st_odgovora, okvir_za_gumbe, gumb)
-         gumb.config(wraplength = 200,  text = besedilo, fg = "white", bg = "grey6",font = ("Comic Sans MS", 20), command = ukaz, height = 30, width = 30)   
+         gumb.config(wraplength = 200,  text = crke[st_odgovora - 2] + ")   " + besedilo, fg = "white", bg = "grey6",font = ("Comic Sans MS", 20), command = ukaz, height = 30, width = 30)   
          return gumb
 
    
@@ -316,7 +390,7 @@ class grafika:
          nepol.grid(sticky = E, row = 1, column = 2, pady = 80, padx = 200)
          self.seznam_widgetov.append(nepol)
       if (self.podatki.glas_ljudstva == True):
-         gumb_glas_ljudstva = tk.Button(okvir_za_gumbe)
+         gumb_glas_ljudstva = tk.Button(okvir_za_gumbe, command = lambda : grafika.izkoristi_glas_ljudstva(self, vprasanje_in_odgovori, gumb_glas_ljudstva))
          gumb_glas_ljudstva.grid(sticky = E, row = 2, column = 2, padx = 200, pady = 80)
          glas_ljudstva_slika = tk.PhotoImage(file = "ozadja/glas_ljudstva.gif")
          gumb_glas_ljudstva.config(image = glas_ljudstva_slika)
@@ -331,7 +405,6 @@ class grafika:
       zacetek.mainloop()
 
 def prva(okno):
-   #okno.resizable(width= False, height = False)
    Grid.rowconfigure(okno,0, weight = 1)
    Grid.columnconfigure(okno,0, weight = 1)
    zazeni_glasbo = True
@@ -356,11 +429,11 @@ def prva(okno):
    gumb_za_zacetek.grid(row = 0, sticky = W)
    izhod = tk.Button(okno, text = "Ne upam :(", fg = "white", bg = "grey6", font = ("Comic Sans MS", 40  ), command = lambda: vmesnik.izhod__(okno))
    izhod.grid(row = 0, sticky = E)
-   vmesnik.seznam_widgetov.append(gumb_za_zacetek)
-   vmesnik.seznam_widgetov.append(izhod)
+   vmesnik.dodaj_na_seznam_widgetov(gumb_za_zacetek, izhod)
    okno.mainloop()
 
 
 
 okno = tk.Tk()
+okno.title("Milijonar")
 prva(okno)
